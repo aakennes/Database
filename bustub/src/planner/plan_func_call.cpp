f75c78b3dcbcfff9f2b23b04f6618cb78e7adfb8
aakennes <rsq363570@outlook.com>
2024-03-28 17:39:24 +0800

    some trick on p0t2

--- bustub/src/planner/plan_func_call.cpp
+++ bustub/src/planner/plan_func_call.cpp
@@ -30,7 +30,7 @@ namespace bustub {
 // 3. return a `StringExpression` std::shared_ptr.
 auto Planner::GetFuncCallFromFactory(const std::string &func_name, std::vector<AbstractExpressionRef> args)
     -> AbstractExpressionRef {
-  if (args.size() != 1 || (func_name != "lower" && func_name != "upper") || typeid(args[0]) != typeid(std::string)) {
+  if (args.size() != 1 || (func_name != "lower" && func_name != "upper") || args[0]->GetReturnType().GetType() != TypeId::VARCHAR ) {
     throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
   }
   StringExpressionType String_Type;
